8d5ebd6e97589f4f2006f5071fea47741bb03bcf
Eva-Sorensen <eva.elaine.sorensen@gmail.com>
2021-09-04 17:29:59 +1200

    added L1Z1X class

---
+++ src/Factions/L1Z1X.java
@@ -0,0 +1,22 @@
+package Factions;
+
+import Units.Unit;
+import Units.UnitNames;
+
+public class L1Z1X extends Faction{
+    private Unit flagship;
+
+    public L1Z1X(){
+        super();
+        //flagship
+        flagship = new Unit.Builder(UnitNames.FLAGSHIP)
+                .addSpaceCombatValue(5,2)
+                .build();
+        //special units
+        dreadnought.setSpaceCombatValue(5,1);
+        dreadnought.setBombardmentValue(5,1);
+        dreadnoughtUpgrade.setSpaceCombatValue(4,1);
+        dreadnoughtUpgrade.setBombardmentValue(4,1);
+        mech.setBombardmentValue(8,1);
+    }
+}
