ece1d12c3852023679ecf77c34e6efbf467633ea
Eva-Sorensen <eva.elaine.sorensen@gmail.com>
2021-09-04 15:59:36 +1200

    added Barony class

---
+++ src/Factions/BaronyOfLetnev.java
@@ -0,0 +1,17 @@
+package Factions;
+
+import Units.Unit;
+import Units.UnitNames;
+
+public class BaronyOfLetnev extends Faction{
+    private Unit flagship;
+
+    public BaronyOfLetnev(){
+        super();
+        //flagship
+        flagship = new Unit.Builder(UnitNames.FLAGSHIP)
+                .addSpaceCombatValue(5,2)
+                .addBombardmentValue(5,3)
+                .build();
+    }
+}
