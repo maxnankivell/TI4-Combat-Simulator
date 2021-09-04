efc1993f84fcfbf3a4493fa4bab6d1e0b674af2a
Eva-Sorensen <eva.elaine.sorensen@gmail.com>
2021-09-04 16:46:12 +1200

    added Muaat class

---
+++ src/Factions/Muaat.java
@@ -0,0 +1,20 @@
+package Factions;
+
+import Units.Unit;
+import Units.UnitNames;
+
+public class Muaat extends Faction{
+    private Unit flagship;
+
+    public Muaat(){
+        super();
+        //flagship
+        flagship = new Unit.Builder(UnitNames.FLAGSHIP)
+                .addSpaceCombatValue(5,2)
+                .build();
+        //special units
+        //Other players' units in this system lose Planetary Shield.
+        warsun.setSpaceCombatValue(3,3);
+        warsun.setBombardmentValue(3,3);
+    }
+}
--- src/Factions/SardakkNorr.java
+++ src/Factions/SardakkNorr.java
@@ -21,21 +21,21 @@ public class SardakkNorr extends Faction{
     }
 
     private void unrelenting() {
-        flagship.setHitValueSpaceCombat(flagship.getHitValueSpaceCombat()+1);
-        warsun.setHitValueSpaceCombat(warsun.getHitValueSpaceCombat()+1);
-        dreadnought.setHitValueSpaceCombat(dreadnought.getHitValueSpaceCombat()+1);
-        dreadnoughtUpgrade.setHitValueSpaceCombat(dreadnoughtUpgrade.getHitValueSpaceCombat()+1);
-        carrier.setHitValueSpaceCombat(carrier.getHitValueSpaceCombat()+1);
-        carrierUpgrade.setHitValueSpaceCombat(carrierUpgrade.getHitValueSpaceCombat()+1);
-        cruiser.setHitValueSpaceCombat(cruiser.getHitValueSpaceCombat()+1);
-        cruiserUpgrade.setHitValueSpaceCombat(cruiserUpgrade.getHitValueSpaceCombat()+1);
-        destroyer.setHitValueSpaceCombat(destroyer.getHitValueSpaceCombat()+1);
-        destroyerUpgrade.setHitValueSpaceCombat(destroyerUpgrade.getHitValueSpaceCombat()+1);
-        fighter.setHitValueSpaceCombat(fighter.getHitValueSpaceCombat()+1);
-        fighterUpgrade.setHitValueSpaceCombat(fighterUpgrade.getHitValueSpaceCombat()+1);
-        infantry.setHitValueGroundCombat(infantry.getHitValueGroundCombat()+1);
-        infantryUpgrade.setHitValueGroundCombat(infantryUpgrade.getHitValueGroundCombat()+1);
-        mech.setHitValueGroundCombat(mech.getHitValueGroundCombat()+1);
+        flagship.setHitValueSpaceCombat(flagship.getHitValueSpaceCombat()-1);
+        warsun.setHitValueSpaceCombat(warsun.getHitValueSpaceCombat()-1);
+        dreadnought.setHitValueSpaceCombat(dreadnought.getHitValueSpaceCombat()-1);
+        dreadnoughtUpgrade.setHitValueSpaceCombat(dreadnoughtUpgrade.getHitValueSpaceCombat()-1);
+        carrier.setHitValueSpaceCombat(carrier.getHitValueSpaceCombat()-1);
+        carrierUpgrade.setHitValueSpaceCombat(carrierUpgrade.getHitValueSpaceCombat()-1);
+        cruiser.setHitValueSpaceCombat(cruiser.getHitValueSpaceCombat()-1);
+        cruiserUpgrade.setHitValueSpaceCombat(cruiserUpgrade.getHitValueSpaceCombat()-1);
+        destroyer.setHitValueSpaceCombat(destroyer.getHitValueSpaceCombat()-1);
+        destroyerUpgrade.setHitValueSpaceCombat(destroyerUpgrade.getHitValueSpaceCombat()-1);
+        fighter.setHitValueSpaceCombat(fighter.getHitValueSpaceCombat()-1);
+        fighterUpgrade.setHitValueSpaceCombat(fighterUpgrade.getHitValueSpaceCombat()-1);
+        infantry.setHitValueGroundCombat(infantry.getHitValueGroundCombat()-1);
+        infantryUpgrade.setHitValueGroundCombat(infantryUpgrade.getHitValueGroundCombat()-1);
+        mech.setHitValueGroundCombat(mech.getHitValueGroundCombat()-1);
 
     }
 
