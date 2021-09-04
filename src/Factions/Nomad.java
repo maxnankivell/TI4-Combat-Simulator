bc7b629eb3a9ad56fb4de55c35cd3d2f7c008db7
Eva-Sorensen <eva.elaine.sorensen@gmail.com>
2021-09-04 19:05:31 +1200

    added Nomad class

---
+++ src/Factions/Nomad.java
@@ -0,0 +1,25 @@
+package Factions;
+
+import Units.Unit;
+import Units.UnitNames;
+
+public class Nomad extends Faction {
+    private Unit flagship;
+    private Unit flagshipUpgrade;
+
+    public Nomad(){
+        super();
+        //flagship
+        flagship = new Unit.Builder(UnitNames.FLAGSHIP)
+                .addSpaceCombatValue(7,2)
+                .addAFBValue(8,3)
+                .build();
+        //special units
+        flagshipUpgrade = new Unit.Builder(UnitNames.FLAGSHIPUPGRADE)
+                .addSpaceCombatValue(5,2)
+                .addAFBValue(5,3)
+                .build();
+        //While a mech is in a space area during combat, you may use its SUSTAIN DAMAGE ability to cancel
+        // a hit that is produced against your ships in this system.
+    }
+}
