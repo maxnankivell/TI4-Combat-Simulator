29b5b7340fe171ec1b70879c2c4fada79add86d9
Eva-Sorensen <eva.elaine.sorensen@gmail.com>
2021-09-06 02:07:28 +1200

    implemented attacker and defender pre process logic in InvasionController

--- src/Controllers/InvasionController.java
+++ src/Controllers/InvasionController.java
@@ -1,6 +1,14 @@
 package Controllers;
 
+import Factions.JolNar;
+import Factions.NaazRokha;
+import Factions.SardakkNorr;
+import GUIData.AttackerOptions;
+import GUIData.DefenderOptions;
 import Units.Unit;
+import Units.UnitName;
+
+import java.util.ArrayList;
 
 public class InvasionController extends Controller{
 
@@ -54,6 +62,112 @@ public class InvasionController extends Controller{
         }
 
         return numHits;
+    }
+
+    public void attackPreProcess(){
+        //Tekklar legion
+        if (AttackerOptions.isArgentFlightCommanderAttackerCheckbox()){
+            tekklarLegionAttacker();
+        }
+
+        //Sol agent
+        if (AttackerOptions.isSolAgentAttackerCheckbox()){
+            addOneDiceToUnit(CombatType.GROUNDCOMBAT, attacker);
+        }
+
+        //Morale boost
+        if (AttackerOptions.isMoraleBoostAttackerCheckbox()){
+            changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, attacker, -1);
+        }
+
+        //Winnu commander
+        if (AttackerOptions.isWinnuCommanderAttackerCheckbox()){
+            changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, attacker, -2);
+        }
+
+        //Jol Nar mech
+        if (attackersFaction instanceof JolNar && containsName(UnitName.MECH, attacker)){
+            changeHitValueOfAllUnitsOfSpecificType(CombatType.GROUNDCOMBAT, attacker, -1, UnitName.INFANTRY, UnitName.INFANTRYUPGRADE);
+        }
+
+        //Naaz Rokha flagship
+        if (attackersFaction instanceof NaazRokha && containsName(UnitName.FLAGSHIP, attacker)){
+            addDiceToSpecificUnitType(CombatType.GROUNDCOMBAT, attacker, UnitName.MECH, UnitName.MECH);
+        }
+    }
+
+    public void defenderPreProcess(){
+        //Magen defense grid
+        if (DefenderOptions.isMagenDefenseGridDefenderCheckbox()){
+            numHitsDefender++;
+        }
+
+        //Tekklar legion
+        if (DefenderOptions.isTekklarLegionDefenderCheckbox()){
+            tekklarLegionDefender();
+        }
+
+        //Sol agent
+        if (DefenderOptions.isSolAgentDefenderCheckbox()){
+            addOneDiceToUnit(CombatType.GROUNDCOMBAT, defender);
+        }
+
+        //Morale boost
+        if (DefenderOptions.isMoraleBoostDefenderCheckbox()){
+            changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, defender, -1);
+        }
+
+        //Winnu commander
+        if (DefenderOptions.isWinnuCommanderDefenderCheckbox()){
+            changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, defender, -2);
+        }
+
+        //Defending in nebula
+        if (DefenderOptions.isDefendingInNebulaDefenderCheckbox()){
+            changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, defender, -1);
+        }
+
+        //Jol Nar mech
+        if (defendersFaction instanceof JolNar && containsName(UnitName.MECH, defender)){
+            changeHitValueOfAllUnitsOfSpecificType(CombatType.GROUNDCOMBAT, defender, -1, UnitName.INFANTRY, UnitName.INFANTRYUPGRADE);
+        }
+
+        //Naaz Rokha flagship
+        if (defendersFaction instanceof NaazRokha && containsName(UnitName.FLAGSHIP, defender)){
+            addDiceToSpecificUnitType(CombatType.GROUNDCOMBAT, defender, UnitName.MECH, UnitName.MECH);
+        }
+    }
+
+    public void attackerMainProcess(){
+        //Fire team
+    }
+
+    public void defenderMainProcess(){
+        //Fire team
+    }
+
+    /**
+     * subtracts one from all attacker ground units hit values
+     * if the defender is Sardakk Norr then add one to
+     * all defender ground units hit values
+     */
+    public void tekklarLegionAttacker(){
+        changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, attacker, -1);
+        if (defendersFaction instanceof SardakkNorr){
+            changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, defender, 1);
+        }
+    }
+
+    /**
+     * subtracts one from all defender ground units hit values
+     * if the attacker is Sardakk Norr then add one to
+     * all attacker ground units hit values
+     */
+    public void tekklarLegionDefender(){
+        changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, defender, -1);
+        if (attackersFaction instanceof SardakkNorr){
+            changeHitValueOfAllUnits(CombatType.GROUNDCOMBAT, attacker, 1);
+        }
     }
 
 }
